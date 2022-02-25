44d742bbab7995153d21b2e7e5058c287061c8c9
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-02-25 13:58:54 -0800

    Update argox.py

--- argox.py
+++ argox.py
@@ -2,13 +2,13 @@ import serial
 from time import sleep
 
 ser = serial.Serial(
-    port="COM7",
+    port="COM31",
     baudrate=9600,
     timeout=0.05
 )
 
 serP = serial.Serial(
-    port="COM31",
+    port="COM7",
     baudrate=9600,
     timeout=0.05
 )
@@ -31,28 +31,34 @@ while True:
         fltData = float(strData)
         print(strData, fltData)
 
+        unit = 'A290,290,0,4,2,2,N,"g"\r\n'
+        datePrint = 'A30,60,0,4,1,1,N,TDyy-me-dd\r\n'
+        timePrint = 'A250,60,0,4,1,1,N,TTh:m:s\r\n'
+        mat = 'A20,120,0,4,1,1,N,"MATERIAL : VIT 07 15 SCI"\r\n'
+        label = 'A70,0,0,4,2,2,N,"INDOKUAT"\r\n'
+        SKU  = 'A20,150,0,4,1,1,N,"SKU  : CHOCHO: 15 TON"\r\n'
+        OPR  = 'A20,180,0,4,1,1,N,"OPR  : KTB/TSM/JRT/WHY"\r\n'
+        LINE = 'A20,210,0,4,1,1,N,"LINE : SCI"\r\n'
+        LOT  = 'A20,240,0,4,1,1,N,"LOT  : 210801216"\r\n'
+
+        serP.write(label.encode())
+        serP.write(mat.encode())
+        serP.write(SKU.encode())
+        serP.write(OPR.encode())
+        serP.write(LINE.encode())
+        serP.write(LOT.encode())
+
         if fltData >= 1000.00:
             value = strData
-            gabung = 'A50,190,0,4,2,2,N,"' + value + '"\r\n'
-            unit = 'A290,190,0,4,2,2,N,"g"\r\n'
-            datePrint = 'A30,40,0,4,1,1,N,TDyy-me-dd\r\n'
-            timePrint = 'A250,40,0,4,1,1,N,TTh:m:s\r\n'
+            gabung = 'A50,290,0,4,2,2,N,"' + value + '"\r\n'
 
-            serP.write(unit.encode())
-            serP.write(gabung.encode())
-            serP.write(timePrint.encode())
-            serP.write(datePrint.encode())
-            serP.write('P1\r\n'.encode())
         else:
             value = strData
-            gabung = 'A80,190,0,4,2,2,N,"' + value + '"\r\n'
-            unit = 'A290,190,0,4,2,2,N,"g"\r\n'
-            datePrint = 'A30,40,0,4,1,1,N,TDyy-me-dd\r\n'
-            timePrint = 'A250,40,0,4,1,1,N,TTh:m:s\r\n'
+            gabung = 'A80,290,0,4,2,2,N,"' + value + '"\r\n'
 
-            serP.write(unit.encode())
-            serP.write(gabung.encode())
-            serP.write(timePrint.encode())
-            serP.write(datePrint.encode())
-            serP.write('P1\r\n'.encode())
+        serP.write(unit.encode())
+        serP.write(timePrint.encode())
+        serP.write(datePrint.encode())
+        serP.write(gabung.encode())
+        serP.write('P1\r\n'.encode())
     sleep(1)
